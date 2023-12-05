260e78f94d817cef5e9ded2aef2726ad1c3fcc23
jeevan5151 <jeevanbabu5151@gmail.com>
2023-12-05 09:44:09 +0530

    Changes have been committed

---
+++ Rotatearraymain.java
@@ -0,0 +1,14 @@
+package project2;
+
+public class Rotatearraymain {
+
+
+	public static void main(String[] args) {
+		RotateArray r = new RotateArray();
+        		int arr[] = { 1, 2, 3, 4, 5, 6, 7 }; 
+        		r.rotate(arr, 5); 
+        		for(int i=0;i<arr.length;i++){
+            			System.out.print(arr[i]+" ");
+        		}
+	}
+}
